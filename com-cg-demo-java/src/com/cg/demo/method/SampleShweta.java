8eeb4e2e091a098c565ff8f20ae242eead2ef2ce
VenumadhavMamidala <venukanni38448@gmail.com>
2021-06-08 23:43:32 +0530

    commit

---
+++ com-cg-demo-java/src/com/cg/demo/method/SampleShweta.java
@@ -0,0 +1,18 @@
+package com.cg.demo.method;
+
+import java.util.Scanner;
+
+public class SampleShweta {
+public static void main(String[] args) {
+	System.out.println("char array");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter size");
+	int size=sc.nextInt();
+	char[] ch=new char[size];
+	System.out.println(ch);
+	System.out.println("enter elements");
+	for(int i=0;i<ch.length;i++) {
+		int[] c=sc.charAt(i);
+	}
+	}
+}
